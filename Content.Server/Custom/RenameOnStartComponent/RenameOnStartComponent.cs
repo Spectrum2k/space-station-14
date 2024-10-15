04bd03472c36bc04cb02c8ea44747e7390ea7f60
Yaroslav Yudaev <spectrum@althub.pro>
2024-10-16 01:52:49 +0300

    + component RenameOnStart + clown

---
+++ Content.Server/Custom/RenameOnStartComponent/RenameOnStartComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Server.Custom.RenameOnStartComponent;
+
+[RegisterComponent]
+public sealed partial class RenameOnStartComponent : Component
+{
+
+}
---
+++ Content.Server/Custom/RenameOnStartComponent/RenameOnStartSystem.cs
@@ -0,0 +1,27 @@
+﻿using Content.Server.Administration;
+using Robust.Shared.Player;
+
+namespace Content.Server.Custom.RenameOnStartComponent;
+
+public sealed class RenameOnStartSystem : EntitySystem
+{
+    [Dependency] private readonly MetaDataSystem _metaSystem = default!;
+    [Dependency] private readonly QuickDialogSystem _quickDialog = default!;
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<RenameOnStartComponent, PlayerAttachedEvent>(OnPlayerAttached);
+    }
+
+    private void OnPlayerAttached(EntityUid ent, RenameOnStartComponent component, PlayerAttachedEvent message)
+    {
+        _quickDialog.OpenDialog(message.Player,
+            "Character Rename",
+            "New name:",
+            (LongString newDescription) =>
+            {
+                _metaSystem.SetEntityName(ent, newDescription.String);
+            });
+
+        RemComp<RenameOnStartComponent>(ent);
+    }
+}
